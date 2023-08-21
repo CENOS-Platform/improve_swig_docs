562d4abcc7f04cebf7d3ebc6f3ae5430463ca92e
vadims.geza <vg@cenos-platform.com>
2023-08-21 12:28:46 +0300

    Add more cenos stuff to settings

--- cenos_settings.py
+++ cenos_settings.py
@@ -10,9 +10,9 @@ class_prefix = {"TopoEntity": "cenos.topologyPy.TopoEntity"}
 basic_types = {"int", "float", "str", "bytes", "bool"}
 
 # location to inject typing string
-typing_inject_location = "import __builtin__"
+typing_inject_location = "# the SWIG interface file instead."
 typing_header = """
-from typing import Sequence,Tuple,Iterator
+from __future__ import annotations 
 """
 
 # conversions from SWIG docstrings to RST docstrings
@@ -33,6 +33,11 @@ to_python_types = {
     "domainGroupVector": "list[DomainGroup]",
     "boundaryGroupVector": "list[BoundaryGroup]",
     "lumpedElementVector": "list[LumpedElement]",
+    "std::map< std::shared_ptr< TopoEntity >,TopoDS_Shape >::key_type const &": "dict[TopoEntity, TopoDS_Shape]",
+    "GeometryData::Mode": "GeometryData.Mode",
+    "LengthUnit": "LengthUnit",
+    "Group": "Group",
+    "MeshStats": "MeshStats",
     # "std::vector< std::shared_ptr< BoundaryGroup >,std::allocator< std::shared_ptr< BoundaryGroup > > >": "BoundaryGroup",
     # "std::shared_ptr< GeometryData >": "GeometryData",
 }
@@ -50,28 +55,44 @@ to_python_type_hints = {
     "swig::SwigPyIterator": "Iterator",
     "swig::SwigPyIterator *": "Iterator",
     "std::shared_ptr< DomainGroup >": "DomainGroup",
+    "DomainGroup_": "DomainGroup",
     "ShapeStats": "ShapeStats",
     "std::vector< std::shared_ptr< TopoEntity >,std::allocator< std::shared_ptr< TopoEntity > > >": "list[TopoEntity]",
     "std::vector< std::shared_ptr< TopoEntity >,std::allocator< std::shared_ptr< TopoEntity > > > *": "list[TopoEntity]",
     "std::shared_ptr< BoundaryGroup >": "BoundaryGroup",
+    "BoundaryGroup_": "BoundaryGroup",
+    "TopoEntity_": "TopoEntity",
+    "std::shared_ptr< LumpedElement >": "LumpedElement",
     "TopoDS_Shape": "TopoDS_Shape",
+    "std::vector< TopoDS_Shape >::value_type": "TopoDS_Shape",
+    "std::vector< TopoDS_Shape >::value_type const &": "TopoDS_Shape",
+    "std::map< std::shared_ptr< TopoEntity >,TopoDS_Shape >::mapped_type const &": "TopoDS_Shape",
+    "std::vector< TopoDS_Shape,std::allocator< TopoDS_Shape > > *": "list[TopoDS_Shape]",
+    "std::vector< DomainGroup_,std::allocator< DomainGroup_ > >": "list[DomainGroup]",
     "std::vector< std::shared_ptr< DomainGroup >,std::allocator< std::shared_ptr< DomainGroup > > >": "list[DomainGroup]",
     "std::vector< std::shared_ptr< DomainGroup >,std::allocator< std::shared_ptr< DomainGroup > > > *": "list[DomainGroup]",
     "std::vector< double,std::allocator< double > >": "list[double]",
     "std::vector< TopoDS_Shape,std::allocator< TopoDS_Shape > >": "list[TopoDS_Shape]",
     "std::vector< gp_Pnt,std::allocator< gp_Pnt > >": "list[gp_Pnt]",
-    "std::vector< std::shared_ptr< BoundaryGroup >,std::allocator< std::shared_ptr< BoundaryGroup > > >": "BoundaryGroup",
-    "std::vector< std::shared_ptr< BoundaryGroup >,std::allocator< std::shared_ptr< BoundaryGroup > > > *": "BoundaryGroup",
+    "std::vector< std::shared_ptr< BoundaryGroup >,std::allocator< std::shared_ptr< BoundaryGroup > > >": "list[BoundaryGroup]",
+    "std::vector< std::shared_ptr< BoundaryGroup >,std::allocator< std::shared_ptr< BoundaryGroup > > > *": "list[BoundaryGroup]",
+    "std::vector< BoundaryGroup_,std::allocator< BoundaryGroup_ > >": "list[BoundaryGroup]",
     "std::vector< std::shared_ptr< TopoEntity > >::value_type const &": "list[TopoEntity]",
     "std::vector< std::shared_ptr< Group >,std::allocator< std::shared_ptr< Group > > >": "list[Group]",
     "std::vector< std::shared_ptr< Group >,std::allocator< std::shared_ptr< Group > > > *": "list[Group]",
     "std::vector< std::shared_ptr< LumpedElement >,std::allocator< std::shared_ptr< LumpedElement > > >": "list[LumpedElement]",
     "std::vector< std::shared_ptr< LumpedElement >,std::allocator< std::shared_ptr< LumpedElement > > > *": "list[LumpedElement]",
+    "std::vector< TopoEntity_,std::allocator< TopoEntity_ > >": "list[TopoEntity]",
+    "TopoDS_ShapeVector": "list[TopoDS_Shape]",
+    "topoentityShapeMap": "dict[TopoEntity, TopoDS_Shape]",
     "TopTools_ListOfShape": "TopTools_ListOfShape",
     "gp_Pln": "gp_Pln",
     "gp_Pnt": "gp_Pnt",
     "gp_Dir": "gp_Dir",
     "gp_Ax1": "gp_Ax1",
+    "ShapeType": "ShapeType",
+    "LengthUnit": "LengthUnit",
+    "std::pair< gp_Pnt,gp_Pnt >": "tuple[gp_Pnt, gp_Pnt]",
     "std::map< std::string,std::vector< std::string > >::mapped_type": "list[str]",
     "std::vector< std::shared_ptr< DomainGroup > >::value_type": "DomainGroup",
     "std::vector< std::shared_ptr< DomainGroup > >::value_type const &": "DomainGroup",
@@ -81,9 +102,13 @@ to_python_type_hints = {
     "std::vector< std::shared_ptr< LumpedElement > >::value_type const &": "LumpedElement",
     "std::vector< std::shared_ptr< Group > >::value_type const &": "Group",
     "std::vector< std::shared_ptr< Group > >::value_type": "Group",
+    "std::shared_ptr< Group >": "Group",
     "std::vector< std::shared_ptr< TopoEntity > >::value_type": "TopoEntity",
     "std::map< std::string,std::vector< std::string,std::allocator< std::string > >,std::less< std::string >,std::allocator< std::pair< std::string const,std::vector< std::string,std::allocator< std::string > > > > >": "dict[str, list[str]]",
-    # "blubla": "BoundaryGroup",
+    "std::map< std::string,std::vector< TopoDS_Shape,std::allocator< TopoDS_Shape > >,std::less< std::string >,std::allocator< std::pair< std::string const,std::vector< TopoDS_Shape,std::allocator< TopoDS_Shape > > > > >": "dict[str, list[TopoDS_Shape]]",
+    "GeometryData::Mode": "GeometryData.Mode",
+    "std::map< TopoEntity_,TopoDS_Shape,std::less< TopoEntity_ >,std::allocator< std::pair< TopoEntity_ const,TopoDS_Shape > > >": "dict[TopoEntity, TopoDS_Shape]", 
+    "std::vector< LumpedElement::Point,std::allocator< LumpedElement::Point > >": "list[LumpedElement.Point]"
     # "std::shared_ptr< GeometryData >": "GeometryData",
     # "std::shared_ptr< TopoEntity >": "TopoEntity",
     # "'std::vector< std::shared_ptr< DomainGroup >,std::allocator< std::shared_ptr< DomainGroup > > >'": "list[DomainGroup]",
